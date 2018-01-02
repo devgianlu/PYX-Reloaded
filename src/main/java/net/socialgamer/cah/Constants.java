85b8f8193e93ec00377aab0829cb443fada2ca36
Gianlu <altomanigianluca@gmail.com>
2018-01-02 23:09:58 +0100

    Working search for games + moved create game to bottom right + toolbar buttons styling (I don't really like them)

--- src/main/java/net/socialgamer/cah/Constants.java
+++ src/main/java/net/socialgamer/cah/Constants.java
@@ -644,7 +644,7 @@ public class Constants {
     public enum GameState implements Localizable {
         DEALING("d", "In Progress"),
         JUDGING("j", "In Progress"),
-        LOBBY("l", "Not Started"),
+        LOBBY("", "Not Started"),
         PLAYING("p", "In Progress"),
         ROUND_OVER("ro", "In Progress");
 
