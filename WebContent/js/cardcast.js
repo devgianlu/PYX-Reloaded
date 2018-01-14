71fd8415a9c87407b7d242f0a20a35b73d925b46
Gianlu <altomanigianluca@gmail.com>
2018-01-14 18:38:15 +0100

    Enabled sorting and filtering

--- WebContent/js/cardcast.js
+++ WebContent/js/cardcast.js
@@ -1,10 +1,11 @@
-function loadDecks() {
+function loadDecks(category, nsfw, sort) {
     const list = new List('cardcast-container', {
         valueNames: ['_name', '_category', '_author', '_sample', {data: ['code']}],
         item: 'cardcast-deck-template'
     });
+    list.clear();
 
-    Cardcast.decks("", "desc", 12, true, 0, "rating", function (data) {
+    Cardcast.decks(category.join(','), getDefaultDirectionFor(sort), 12, nsfw, 0, sort, function (data) {
         const results = data.results.data;
         for (let i = 0; i < results.length; i++) {
             const item = results[i];
@@ -28,32 +29,30 @@ function loadDecks() {
 }
 
 function getCategoryMaterialIconsName(category) {
-    switch (category) {
-        case "books":
-            return "book";
-        case "community":
-            return "people";
-        case "gaming":
-            return "videogame_asset";
-        case "movies":
-            return "movie";
-        case "music":
-            return "music_note";
-        case "sports":
-            return "directions_run";
-        case "technology":
-            return "phonelink";
-        case "television":
-            return "tv";
-        case "translation":
-            return "translate";
-        default:
-        case "other":
-            return "more";
-        case "random":
-            return "casino";
+    for (let i = 0; i < cardcastCategories.length; i++) {
+        const item = cardcastCategories[i];
+        if (category === item.value) return item.icon;
     }
+
+    return "other";
 }
+
+const cardcastSorts = [{print: "Rating", name: "rating", type: "rating", default_direction: "desc"},
+    {print: "Name", name: "name", type: "name", default_direction: "asc"},
+    {print: "Newest", name: "newest", type: "created_at", default_direction: "desc"},
+    {print: "Size", name: "size", type: "card_count", default_direction: "desc"}];
+
+const cardcastCategories = [{name: "Books", icon: "book", value: "books"},
+    {name: "Community", icon: "people", value: "community"},
+    {name: "Gaming", icon: "videogame_asset", value: "gaming"},
+    {name: "Movies", icon: "movie", value: "movies"},
+    {name: "Music", icon: "music_note", value: "music"},
+    {name: "Sports", icon: "directions_run", value: "sports"},
+    {name: "Technology", icon: "phonelink", value: "technology"},
+    {name: "Television", icon: "tv", value: "television"},
+    {name: "Translation", icon: "translate", value: "translation"},
+    {name: "Other", icon: "more", value: "other"},
+    {name: "Random", icon: "casino", value: "random"}];
 
 function createCategoryAndAuthorString(category, author) {
     let str = "";
@@ -155,6 +154,96 @@ function createSamplePhrase(calls, responses) {
     }
 
     return str + randomCall.text[neededResponses];
+}
+
+let _cardcastMenu = undefined;
+
+function showCardcastMenu() {
+    _cardcastMenu.open = !_cardcastMenu.open;
+}
+
+function cardcastOptionsChanged(menu) {
+    loadDecks(getCurrentCardcastCategories(menu.querySelector('.\_categories')), true, getCurrentCardcastSorting(menu.querySelector('.\_sort')));
+}
+
+function getDefaultDirectionFor(sort) {
+    for (let i = 0; i < cardcastSorts.length; i++) {
+        const item = cardcastSorts[i];
+        if (sort === item.value) return item.default_direction;
+    }
+
+    return "desc";
+}
+
+function getCurrentCardcastCategories(categories) {
+    const selected = [];
+    for (let i = 0; i < categories.children.length; i++) {
+        const child = categories.children[i];
+        if (child.querySelector('input').checked) selected.push(child.getAttribute("data-value"));
+    }
+
+    return selected;
+}
+
+function getCurrentCardcastSorting(sort) {
+    for (let i = 0; i < sort.children.length; i++) {
+        const child = sort.children[i];
+        if (child.querySelector('input').checked) return child.getAttribute("data-value");
+    }
+
+    return "rating";
+}
+
+function setupCardcastMenu(menuElm) {
+    if (_cardcastMenu === undefined) {
+        _cardcastMenu = new mdc.menu.MDCSimpleMenu(menuElm);
+        _cardcastMenu.listen('MDCSimpleMenu:selected', () => cardcastOptionsChanged(menuElm));
+        _cardcastMenu.listen('MDCSimpleMenu:cancel', () => cardcastOptionsChanged(menuElm));
+    }
+
+    const categories = new List(menuElm, {
+        item: 'category-item-template',
+        listClass: '_categories',
+        valueNames: ['_name', '_icon', {data: ['value']}]
+    });
+    categories.clear();
+
+    for (let i = 0; i < cardcastCategories.length; i++) {
+        const category = cardcastCategories[i];
+        const elm = categories.add({
+            "_name": category.name,
+            "_icon": category.icon,
+            "value": category.value
+        })[0];
+        elm.elm.querySelector('input').setAttribute("id", "category_" + category.value);
+        elm.elm.querySelector('label').setAttribute("for", "category_" + category.value);
+    }
+
+
+    const sort = new List(menuElm, {
+        item: 'sort-item-template',
+        listClass: '_sort',
+        valueNames: ['_name', {data: ['value']}]
+    });
+    sort.clear();
+
+    for (let i = 0; i < cardcastSorts.length; i++) {
+        const orderBy = cardcastSorts[i];
+        const elm = sort.add({"_name": orderBy.print, "value": orderBy.type})[0];
+        const input = elm.elm.querySelector('input');
+        input.setAttribute("id", "sort_" + orderBy.name);
+        elm.elm.querySelector('label').setAttribute("for", "sort_" + orderBy.name);
+
+        if (orderBy.type === "rating") input.checked = true;
+
+        input.addEventListener('change', function () {
+            if (this.checked) {
+                $(menuElm.querySelector('.\_sort')).find("input[id!=sort_" + orderBy.name + "]").each(function () {
+                    $(this)[0].checked = false;
+                });
+            }
+        })
+    }
 }
 
 class Cardcast {
