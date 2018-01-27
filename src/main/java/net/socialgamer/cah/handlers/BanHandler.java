fd67ec4b821e329c567bd6b8e912ef7b64cf371a
M. Gage Morgan <13326489+MGageMorgan@users.noreply.github.com>
2018-01-27 04:30:33 -0500

    Annotated code
    I feel this needs done - code oftentimes can benefit from comments long after we're gone. Yes, you can read it, but it's still good to have some explanation of what's going on.

--- src/main/java/net/socialgamer/cah/handlers/BanHandler.java
+++ src/main/java/net/socialgamer/cah/handlers/BanHandler.java
@@ -25,14 +25,25 @@ public class BanHandler extends BaseHandler {
 
     @Override
     public JsonWrapper handle(User user, Parameters params, HttpServerExchange exchange) throws BaseCahHandler.CahException {
+        // Detect that user doesn't have permission to kick/ban etc
         if (!user.isAdmin()) throw new BaseCahHandler.CahException(ErrorCode.NOT_ADMIN);
 
         String nickname = params.get(AjaxRequest.NICKNAME);
+        
+        // Assuming this is for when the command wasn't properly typed
         if (nickname == null || nickname.isEmpty())
             throw new BaseCahHandler.CahException(ErrorCode.NO_NICK_SPECIFIED);
 
         String banIp;
+        // Single out the user we want to ban, give it its own object
         User kickUser = connectedUsers.getUser(nickname);
+        
+        /*
+         * Assuming singled out user exists, set banIP to the IP(?) of the user in question.
+         * Then, send a message via the LongPoll servlet to the client with a notif they've been kicked.
+         * Remove the user in question from the list of connected users on the server
+         * To everyone else: Send a message of who banned who
+         */
         if (kickUser != null) {
             banIp = kickUser.getHostname();
 
@@ -40,12 +51,12 @@ public class BanHandler extends BaseHandler {
 
             connectedUsers.removeUser(kickUser, DisconnectReason.BANNED);
             logger.info(String.format("Banning %s (%s) by request of %s", kickUser.getNickname(), banIp, user.getNickname()));
-        } else {
+        } else { //Ban via nickname instead of IP address.
             banIp = nickname;
             logger.info(String.format("Banning %s by request of %s", banIp, user.getNickname()));
         }
 
         BanList.add(banIp);
-        return JsonWrapper.EMPTY;
+        return JsonWrapper.EMPTY; //Doesn't return any JSON
     }
 }
