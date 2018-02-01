960ca7323ee232e5a7e73cc8b7f517d0dbd2001c
Gianlu <altomanigianluca@gmail.com>
2018-02-01 14:20:50 +0100

    Using Noty.js + custom theme

---
+++ WebContent/js/notifs.helper.js
@@ -0,0 +1,29 @@
+new Noty({
+    theme: 'mdc',
+    type: 'info',
+    text: 'Some notification text',
+}).show();
+
+new Noty({
+    theme: 'mdc',
+    type: 'alert',
+    text: 'Some notification text',
+}).show();
+
+new Noty({
+    theme: 'mdc',
+    type: 'success',
+    text: 'Some notification text',
+}).show();
+
+new Noty({
+    theme: 'mdc',
+    type: 'warning',
+    text: 'Some notification text',
+}).show();
+
+new Noty({
+    theme: 'mdc',
+    type: 'error',
+    text: 'Some notification text',
+}).show();
