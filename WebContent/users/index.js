20b99e822413b4cceca5d5aecc8a9d43264f13cc
Gianlu <altomanigianluca@gmail.com>
2018-03-07 21:16:24 +0100

    Refresh users page

--- WebContent/users/index.js
+++ WebContent/users/index.js
@@ -7,20 +7,22 @@ class UsersManager {
             item: 'userTemplate',
             valueNames: ['_nick', '_role', {attr: 'src', name: '_img'}]
         });
+
+        this.refresh = $('#refresh');
+        this.refresh.on('click', () => this.loadUsers());
     }
 
-    setup() {
-        this.users.clear();
-
+    loadUsers() {
         Requester.request("gn", {}, (data) => {
             /**
              * @param {object[]} data.nl - Names list
              * @param {string} data.nl[].n - Nickname
              * @param {string} data.nl[].ia - Whether the user is an admin
-             *  @param {string} data.nl[].ha - Whether the user has an account
+             * @param {string} data.nl[].ha - Whether the user has an account
              * @param {string} data.nl[].p - Profile picture
              */
 
+            this.users.clear();
             const list = data.nl;
             for (let i = 0; i < list.length; i++) {
                 const user = list[i];
