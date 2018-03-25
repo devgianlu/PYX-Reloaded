820fc665a4e6983efd69e4b9e1bb0c2dd2ed5471
Gianlu <altomanigianluca@gmail.com>
2018-03-25 12:27:05 +0200

    Fixed bash scripts

--- dist/start.sh
+++ dist/start.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+cd "$(dirname "$0")"
 sudo java -jar ./PYX-Reloaded.jar
--- dist/update.sh
+++ dist/update.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+cd "$(dirname "$0")"
 sudo mv ./PYX-Reloaded.jar ./PYX-Reloaded.old.jar
 sudo java -jar ./PYX-Reloaded.old.jar --update
 sudo rm ./PYX-Reloaded.old.jar
